12ab23ce5475d650a9c4d4315402cf812223887d
Nadiia Maltseva <amanavi2021@gmail.com>
2023-10-29 20:02:22 +0200

    Update 4.ts

--- src/generic/4.ts
+++ src/generic/4.ts
@@ -2,16 +2,23 @@
   Використовуйте generics та інтерфейси, щоб виправити помилку в наступних класах:
 */
 
-class Component {
+interface IComponent {
+  title: string;
+}
+
+class Component<T extends object> {
   constructor (public props:T) {
 
   }
 }
 
-class Page extends Component {
+class Page<T extends IComponent> extends Component<T> {
   pageInfo () {
     console.log(this.props.title);
   }
 }
 
 export {};
+  
+
+
